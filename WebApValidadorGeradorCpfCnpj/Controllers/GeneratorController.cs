0afe2326d9c8379c33ebf3333deca37d0b21c0ff
felipe <phelipejulio16@gmail.com>
2018-09-04 13:39:56 -0300

    Commit inicial

---
+++ WebApValidadorGeradorCpfCnpj/Controllers/GeneratorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApValidadorGeradorCpfCnpj.Fabricas;
+using WebApValidadorGeradorCpfCnpj.Validadores;
+
+namespace WebApValidadorGeradorCpfCnpj.Controllers
+{
+    [RoutePrefix("api/gerador")]
+    public class GeneratorController : ApiController
+    {
+        [Route("cpf")]
+        public HttpResponseMessage GetCpf()
+        {
+            string cpf = CpfFactory.GerarCpf();
+
+            return Request.CreateResponse(HttpStatusCode.OK, cpf);
+        }
+
+        [Route("cnpj")]
+        public HttpResponseMessage GetCnpj()
+        {
+            string cnpj = CnpjFactory.gerarCnpj();
+
+            return Request.CreateResponse(HttpStatusCode.OK, cnpj);
+        }
+
+    }
+}
---
+++ WebApValidadorGeradorCpfCnpj/Controllers/ValidateController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApValidadorGeradorCpfCnpj.Fabricas;
+using WebApValidadorGeradorCpfCnpj.Validadores;
+
+namespace WebApValidadorGeradorCpfCnpj.Controllers
+{
+    [RoutePrefix("api/validar")]
+    public class ValidateController : ApiController
+    {
+        [Route("cnpj/{cnpj}")]
+        [HttpPost]
+        public HttpResponseMessage PostCnpj(string cnpj)
+        {
+            var cnpjValido = CnpjValidator.Validar(cnpj);
+            string mensagem = "";
+            if(cnpjValido)
+            {
+                mensagem = "Cnpj valido";
+            } else
+            {
+                mensagem = "Cnpj invalido";
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, mensagem);
+        }
+
+        [Route("cpf/{cpf}")]
+        [HttpPost]
+        public HttpResponseMessage PostCpf(string cpf)
+        {
+            var cpfValido = CpfValidator.Validar(cpf);
+            string mensagem = "";
+
+            if (cpfValido)
+            {
+                mensagem = "Cpf valido";
+            }
+            else
+            {
+                mensagem = "Cpf invalido";
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, mensagem);
+        }
+    }
+}
